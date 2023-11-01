bdb15c0cd3f28c0f0c3d48840d777319a5b6e3a5
Radko Mishev <rm.mishev@gmail.com>
2023-11-01 09:44:47 +0200

    Remove server; move client one directory up

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# mesembria-new-client
+To run the front-end of the application, simply use
+
+```npm run dev```
+
+Sass files should be compiled automatically!
+
+REMOVE README.md from inslide src/app-common and etc. later on.
--- server/utils/jwt.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-const jwt = require('jsonwebtoken');
-const generateAccessToken = (username,email,verified,user_id) => {
-    return jwt.sign({username, email,verified,user_id}, process.env.PRIVATE_KEY, { expiresIn: process.env.JWT_EXPIRES_IN });
-}
-
-exports.generateAccessToken = generateAccessToken;
