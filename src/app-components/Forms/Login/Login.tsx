fc2540e506b447e06a1932abad2eafc3a7ba304f
Aleks Ivanov <al.ivanov1908@gmail.com>
2023-11-12 01:14:34 +0200

    fix type errors in login form

--- src/app-components/Forms/Login/Login.tsx
+++ src/app-components/Forms/Login/Login.tsx
@@ -6,11 +6,24 @@ type LoginData = {
   password: string;
 };
 
+interface ErrorInfo {
+  hasError: boolean;
+  message: string;
+}
+
+const initialErrors: Record<keyof LoginData, ErrorInfo> = {
+  username: { hasError: false, message: '' },
+  password: { hasError: false, message: '' },
+};
+
 export const Login = () => {
-  const [errors, setErrors] = useState({
-    username: "",
-    password: "",
-  });
+  // const [errors, setErrors] = useState({
+  //   username: "",
+  //   password: "",
+  // });
+
+  const [errors, setErrors] = useState<Record<keyof LoginData, ErrorInfo>>(initialErrors);
+
   const [userData, setUserData] = useState<LoginData>({
     username: "",
     password: "",
@@ -27,18 +40,14 @@ export const Login = () => {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    const newErrors: any = { ...errors };
+    const newErrors = { ...errors };
     if (userData.username.length < 2) {
-      newErrors.username = "Please enter a valid username";
-    } else {
-      newErrors.username = "";
-    }
+      newErrors.username = { hasError: true, message: 'Please enter valid username' };
+    } 
 
     if (userData.password.length === 0) {
-      newErrors.password = "Please enter a valid password";
-    } else {
-      newErrors.password = "";
-    }
+      newErrors.password = { hasError: true, message: 'Please enter valid password' };
+    } 
 
     setErrors(newErrors);
   };
@@ -62,9 +71,9 @@ export const Login = () => {
               maxLength={40}
               value={userData.username}
               onChange={formHandler}
-              className={errors.username ? "error" : ""}
+              className={errors.username.hasError ? "error" : ""}
             />
-            <p>{errors.username ? errors.username : ""}</p>
+            <p className="errorText">{errors.username.hasError ? errors.username.message : ''}</p>
 
             <input
               type="password"
@@ -72,9 +81,9 @@ export const Login = () => {
               placeholder="Password"
               value={userData.password}
               onChange={formHandler}
-              className={errors.password ? "error" : ""}
+              className={errors.password.hasError ? "error" : ""}
             />
-            <p>{errors.password ? errors.password : ""}</p>
+            <p className="errorText">{errors.password.hasError ? errors.password.message : ''}</p>
           </section>
           <div id="buttonWrapper">
             <input type="submit" value={"Submit"} />
