dbf93833d4744142da800c7df41e63f4946a13e5
Aleks Ivanov <al.ivanov1908@gmail.com>
2023-11-17 16:08:13 +0200

    changes to design

--- src/app-components/Dashboard/Dashboard.tsx
+++ src/app-components/Dashboard/Dashboard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 
 export const Dashboard = ({ sidebarOpen }) => {
   return (
@@ -8,9 +9,18 @@ export const Dashboard = ({ sidebarOpen }) => {
         <div className="recent-sets">
           <div className="set-card">
             <div className="set-title">COS2001 C++</div>
-            <div className="set-description">Description of this set of cards and it informs us about fds course</div>
-            <div className="set-creator closed">Aleksandar Ivaylov Ivanov</div>
+            <div className="set-description closed">Description of this set of cards and it informs us about fds course</div>
             <div className="set-institution closed">American University in Bulgaria</div>
+            <div className="set-creator">
+              <div className="creator-img">
+                <span className="image">
+                  <img src="src/app-components/Navigation/logo.png"></img>
+                </span>
+              </div>
+              <div className="creator-name">
+                Aleksandar Ivaylov Ivanov
+              </div>
+            </div>
           </div>
           <div className="set-card">
             <div className="set-title">COS2001 C++</div>
@@ -20,9 +30,9 @@ export const Dashboard = ({ sidebarOpen }) => {
           </div>
           <div className="set-card">
             <div className="set-title">COS2001 C++</div>
-            <div className="set-description">Description of this set of cards and it informs us about fds course</div>
+            <div className="set-description">Description of this set of </div>
             <div className="set-creator closed">Aleksandar Ivaylov Ivanov</div>
-            <div className="set-institution closed">American University in Bulgaria</div>
+            <div className="set-institution ">American University in Bulgaria</div>
           </div>
           <div className="set-card">
             <div className="set-title">COS2001 C++</div>
