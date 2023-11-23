a1a916f6667ad6068a67b5835adbbe4a6d66cf0f
Aleks Ivanov <al.ivanov1908@gmail.com>
2023-11-23 21:29:00 +0200

    fix transition

--- src/app-components/Settings/Settings.tsx
+++ src/app-components/Settings/Settings.tsx
@@ -1,9 +1,14 @@
+import { useAppSelector } from "../../app-context/store";
+
 export const Settings = () => {
+
+    const navigationSliceManager = useAppSelector((state) => state.navigationReducer);
+
     return(
-        <div>
-            <h1>
-                Settings
-            </h1>
+        <div className={`container ${navigationSliceManager.open ? "open" : "close"}`}>
+            <div className="content">
+                <h1>Settings</h1>
+            </div>
         </div>
     )
 }
