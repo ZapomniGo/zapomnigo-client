0ebafc14e20ddeb4f7a393ac08242c177321b559
Radko Mishev <mishev.radko@ibm.com>
2023-12-03 12:55:04 +0200

    (fix): Remove unused import

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { homeRoute, registerRoute, setsRoute, foldersRoute, createRoute, settingsRoute, loginRoute } from "./app-utils/AppRoutes";
+import {
+  homeRoute,
+  registerRoute,
+  setsRoute,
+  foldersRoute,
+  createRoute,
+  settingsRoute,
+  loginRoute,
+} from "./app-utils/AppRoutes";
 import { Navigation } from "./app-components/Navigation/Navigation";
 import { Provider } from "react-redux";
 import { Sets } from "./app-components/Sets/Sets";
@@ -12,7 +20,6 @@ import "./index.scss";
 import { Create } from "./app-components/Create/Create";
 import { Settings } from "./app-components/Settings/Settings";
 import { MainPage } from "./app-components/MainPage/MainPage";
-import { App } from "./App";
 import "./index.scss";
 import { Registration } from "./app-components/Forms/Registration/Registration";
 import { Login } from "./app-components/Forms/Login/Login";
@@ -21,47 +28,46 @@ const queryClient = new QueryClient();
 const router = createBrowserRouter([
   {
     path: homeRoute,
-    element: <Navigation/>
-  ,
-  children: [
-    {
-      path: homeRoute,
-      element: <MainPage/>
-    },
-    {
-      path: setsRoute,
-      element: <Sets/>  
-    },
-    {
-      path: foldersRoute,
-      element: <Folders/>
-    },
-    {
-      path: createRoute,
-      element: <Create/>
-    },
-    {
-      path: settingsRoute,
-      element: <Settings/>
-    }
-
-  ]},
+    element: <Navigation />,
+    children: [
+      {
+        path: homeRoute,
+        element: <MainPage />,
+      },
+      {
+        path: setsRoute,
+        element: <Sets />,
+      },
+      {
+        path: foldersRoute,
+        element: <Folders />,
+      },
+      {
+        path: createRoute,
+        element: <Create />,
+      },
+      {
+        path: settingsRoute,
+        element: <Settings />,
+      },
+    ],
+  },
   {
     path: registerRoute,
     element: <Registration />,
   },
   {
     path: loginRoute,
-    element: <Login/>
-  }
+    element: <Login />,
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
-  </React.StrictMode>
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </React.StrictMode>
   </Provider>
 );
