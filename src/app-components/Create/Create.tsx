36413a568f0fc0913bcce4a1935c9cd8ba5f4db3
Aleks Ivanov <al.ivanov1908@gmail.com>
2023-12-29 17:15:55 +0200

    changes on reponsive and fix main page container

--- src/app-components/Create/Create.tsx
+++ src/app-components/Create/Create.tsx
@@ -17,6 +17,9 @@ export const Create = () =>{
                         <div className="option-title">
                             <h2>Създай сет</h2>
                         </div>
+                        <div className="option-description">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quaerat amet sunt! Ad soluta quis iste consectetur odio, itaque, dolore nam et voluptates incidunt neque.</p>
+                        </div>
                     </a>
                     <a href="/create-folder" className="create-folder-option">
                         <div className="option-svg">
@@ -25,6 +28,9 @@ export const Create = () =>{
                         </div>
                         <div className="option-title">
                         <h2>Създай папка</h2>
+                        </div>
+                        <div className="option-description">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quaerat amet sunt! Ad soluta quis iste consectetur odio, itaque, dolore nam et voluptates incidunt neque.</p>
                         </div>
                     </a>
                 </div>
