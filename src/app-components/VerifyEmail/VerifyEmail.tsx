54c583626136e90fd5069001e08e86787c88dfcc
Atanas Bobev <bobevatanas@gmail.com>
2023-12-31 18:22:51 +0200

    Refactor VerifyEmail component and styles

--- src/app-components/VerifyEmail/VerifyEmail.tsx
+++ src/app-components/VerifyEmail/VerifyEmail.tsx
@@ -1,14 +1,13 @@
-import React from 'react'
 
 const VerifyEmail = () => {
   return (
-    <section>
-        <div className="container">
+    <div className="verify">
+        <div className="verifyContainer">
                     <h1>Потвърдете имейла си</h1>
                     <p>Проверете имейла си, би трябвало да сте получили имейл от нас на адрес___. Проверете и спам папката. Ако все пак не сте получили, натиснете бутона отдолу</p>
             <button className="btn btn-primary">Изпрати отново</button>
             </div>
-    </section>
+    </div>
   )
 }
 
