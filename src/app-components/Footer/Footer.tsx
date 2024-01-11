72d5390ee34770cabf796a5b4dfbb130b62cb5ae
Atanas Bobev <bobevatanas@gmail.com>
2024-01-11 18:38:23 +0200

    footer styling fixes

--- src/app-components/Footer/Footer.tsx
+++ src/app-components/Footer/Footer.tsx
@@ -3,50 +3,43 @@ import { BsInstagram } from "react-icons/bs";
 import { MdEmail } from "react-icons/md";
 import { SiGmail } from "react-icons/si";
 import { RiTwitterXLine } from "react-icons/ri";
-
-
+import Tilty from "react-tilty";
 export const Footer = () => {
-    return (
-        <div id="footer">
-            <div className="col-1">
-                <div className="footer-logo">
-                    <img src="src/app-components/Footer/image.png" alt="" />
-                </div>
-            </div>
-            <div className="col-2">
-                <div className="about-us">
-                    <h2>Общи условия</h2>
-                </div>
-                <div className="options">
-                    <ul>
-                        <li>
-                        <a href="">Политика за поверителност</a>
-                        </li>
-                        <li>
-                            <a href="">Политика за ползване</a>
-                        </li>
-                        <li>
-                            <a href="">Маркетингово съгласие</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div className="social-media">
-                <div className="options">
-                    <a href="#">
-                        <FaFacebookSquare />
-                    </a>
-                    <a href="#">
-                        <BsInstagram />
-                    </a>
-                    <a href="#" className="">
-                        <SiGmail />
-                    </a>
-                    <a href="#">
-                        <RiTwitterXLine />
-                    </a>                    
-                </div>
-                {/* <div className="options">
+  return (
+    <div id="footer">
+      <div onClick={()=>window.location.replace('https://www.zapomnigo.com')} className="col-1">
+        <Tilty glare={true} max={20}>
+          <div className="footer-logo">
+            <img src="src/app-components/Footer/image.png" alt="" />
+          </div>
+        </Tilty>
+      </div>
+      <div className="col-2">
+        <div className="options">
+          <a className="link" href="/legal/terms-of-service">
+            Общи условия
+          </a>
+          <a className="link" href="/legal/cookie-rules">
+            Политика за бисквитки
+          </a>
+          <a className="link" href="/legal/privacy-policy">
+            Политка за поверителност
+          </a>
+        </div>
+      </div>
+      <div className="social-media">
+        <div className="options">
+          <a href="www.facebook.com">
+            <FaFacebookSquare />
+          </a>
+          <a href="https://www.instagram.com/zapomnigo">
+            <BsInstagram />
+          </a>
+          <a href="mailto:zapomnigo.com@gmail.com" className="">
+            <SiGmail />
+          </a>
+        </div>
+        {/* <div className="options">
                     <a href="#" className="">
                         <SiGmail />
                     </a>
@@ -55,8 +48,8 @@ export const Footer = () => {
                     </a>
                     
                 </div> */}
-            </div>
-            {/* <div className="col-3">
+      </div>
+      {/* <div className="col-3">
                 <div className="about-us">
                     <h2 >About us</h2>
                 </div>    
@@ -92,6 +85,6 @@ export const Footer = () => {
                     </ul>
                 </div>
             </div> */}
-        </div>
-    );
-}
+    </div>
+  );
+};
