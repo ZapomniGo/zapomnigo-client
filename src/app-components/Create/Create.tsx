bfb9c3c5a4fc160a4edd8958b35b5a92a7fd5821
Atanas Bobev <bobevatanas@gmail.com>
2024-01-12 21:39:43 +0200

    fixed small bug

--- src/app-components/Create/Create.tsx
+++ src/app-components/Create/Create.tsx
@@ -1,34 +1,37 @@
 import { FaRegFolderOpen } from "react-icons/fa";
 import { TbCards } from "react-icons/tb";
 import Dashboard from "../Dashboard/Dashboard";
+import Tilty from "react-tilty";
 
-export const Create = () =>{
-    return (
-        <Dashboard>
-        <div className="create-section">
-           <div className="create-header">
-                <div className="create-option">
-                    <a href="/create-set" className="create-set-option">
-                        <div className="option-svg">
-                            <TbCards/>
-                        </div>
-                        <div className="option-title">
-                            <h2>Тесте с флашкарти</h2>
-                        </div>
-                    </a>
-                    <a href="/create-folder" className="create-folder-option">
-                        <div className="option-svg">
-                        <FaRegFolderOpen/>
-
-                        </div>
-                        <div className="option-title">
-                        <h2>Папка</h2>
-                        </div>
-                     
-                    </a>
+export const Create = () => {
+  return (
+    <Dashboard>
+      <div className="create-section">
+        <div className="create-header">
+          <div className="create-option">
+              <a href="/create-set" className="create-set-option">
+              <Tilty className="tilty" max={10} glare={true} perspective={500} reverse={true}>
+                <div className="option-svg">
+                  <TbCards />
                 </div>
-            </div>
+                <div className="option-title">
+                  <h2>Тесте с флашкарти</h2>
+                </div>
+                </Tilty>
+              </a>
+            <a href="/create-folder" className="create-folder-option">
+              <Tilty className="tilty" max={10} glare={true} perspective={500} reverse={true}>
+                <div className="option-svg">
+                  <FaRegFolderOpen />
+                </div>
+                <div className="option-title">
+                  <h2>Папка</h2>
+                </div>
+              </Tilty>
+            </a>
+          </div>
         </div>
-        </Dashboard>
-    )
-}
+      </div>
+    </Dashboard>
+  );
+};
