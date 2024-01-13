7aca60c203dda678f2fb1ee375ed2408ffa6a4a5
Radko Mishev <rm.mishev@gmail.com>
2024-01-13 16:28:01 +0200

    Test logo visibility 2

--- src/app-components/MainPage/MainPage.tsx
+++ src/app-components/MainPage/MainPage.tsx
@@ -52,7 +52,7 @@ export const MainPage: React.FC = () => {
                 title={card.set_name}
                 description={card.set_description}
                 institution={card.organization_name}
-                image={'/logo.jpg'}
+                image={'public/logo.jpg'}
                 creator_name={card.username}
                 onMouseEnter={handleMouseEnter}
                 onMouseLeave={handleMouseLeave}
