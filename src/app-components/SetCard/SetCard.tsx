cc236a7a9d3592b7fed8e807655185239a1a0fea
Atanas Bobev <bobevatanas@gmail.com>
2024-01-14 14:39:16 +0200

    Update favicon and truncate long titles and names

--- src/app-components/SetCard/SetCard.tsx
+++ src/app-components/SetCard/SetCard.tsx
@@ -12,61 +12,84 @@ interface SetCardProps {
   creator_name: string;
 }
 
-const SetCard: React.FC<SetCardProps> = ({ id, title, description, institution, image, creator_name, onMouseEnter, onMouseLeave, isSelected, category }) => {
+const SetCard: React.FC<SetCardProps> = ({
+  id,
+  title,
+  description,
+  institution,
+  image,
+  creator_name,
+  onMouseEnter,
+  onMouseLeave,
+  isSelected,
+  category,
+}) => {
   const displayDescription = () => {
     let n = 120;
     if (description.length > n) {
-        while (description[n] != ' ') {
-            n -= 1;
-        }
-        return <p>{description.slice(0, n)}...</p>;
+      while (description[n] != " ") {
+        n -= 1;
+      }
+      return <p>{description.slice(0, n)}...</p>;
     }
     return <p>{description}</p>;
   };
   const navigate = useNavigate();
-  
+
   return (
     <div
-      className={"set-card"+(isSelected ? " active" : "")}
+      className={"set-card" + (isSelected ? " active" : "")}
       onMouseEnter={() => onMouseEnter(id)}
       onMouseLeave={onMouseLeave}
       onClick={() => navigate(`/set/${id}`)}
     >
       <div className={`title-options ${isSelected ? "open" : "close"}`}>
-      <div className={`set-title ${isSelected ? "open" : "close"}`}>
-        {title.length > 34 ? title.substring(0, 35) + '...' : title}
-      </div>        
-      {/* <div className={`more-options ${isSelected ? "open" : "open"}`}>
+        <div className={`set-title ${isSelected ? "open" : "close"}`}>
+          {title.length > 34 ? title.substring(0, 35) + "..." : title}
+        </div>
+        {/* <div className={`more-options ${isSelected ? "open" : "open"}`}>
           <SlOptionsVertical />
         </div> */}
       </div>
-      
+
       <div className={`set-description ${isSelected ? "open" : "close"}`}>
-        {description.length > 99 ? description.substring(0, 102) + '...' : description}
+        {description.length > 99
+          ? description.substring(0, 102) + "..."
+          : description}
       </div>
       <div className={`set-creator  ${isSelected ? "open" : "close"}`}>
         <div className="image">
-            <img src={image} alt="Лого" />
+          <img src={image} alt="Лого" />
         </div>
         <div className="creator-name">
-            <p>
-              {creator_name}
-            </p>
+          <p>
+            {creator_name.length > 21
+              ? creator_name.substring(0, 21) + "..."
+              : creator_name}
+          </p>
         </div>
         {institution ? (
-            <div className={`set-institution ${isSelected ? "open" : "close"}`}>
-              <a href="#">
-                {institution}
-              </a>
-            </div>
-          ): ''}
-          {category ? (
-            <div className={`set-category ${isSelected ? "open" : "close"}`}>
-              <a className="miniLabel">
-                {category}
-              </a>
-            </div>
-          ): ''}
+          <div className={`set-institution ${isSelected ? "open" : "close"}`}>
+            <a href="#">
+              {institution.length > 10
+                ? institution.substring(0, 21) + "..."
+                : institution}
+            </a>
+          </div>
+        ) : (
+          ""
+        )}
+        {category ? (
+          <div className={`set-category ${isSelected ? "open" : "close"}`}>
+            <a className="miniLabel">
+              {category.length > 10
+                ? category.substring(0, 21) + "..."
+                : category}
+            </a>
+          </div>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
