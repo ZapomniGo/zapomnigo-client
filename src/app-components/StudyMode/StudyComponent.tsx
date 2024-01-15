f40db070689e5d17a98f6ac602acfacff4a17ee2
Radko Mishev <rm.mishev@gmail.com>
2024-01-15 03:52:42 +0200

    Changes (fix shuffleWeightedArray)

--- src/app-components/StudyMode/StudyComponent.tsx
+++ src/app-components/StudyMode/StudyComponent.tsx
@@ -7,14 +7,14 @@ import parse from "html-react-parser";
 import Dashboard from "../Dashboard/Dashboard";
 
 const StudyComponent = () => {
-  const [flashcards, setFlashcards] = useState({
-    set_name: "",
-    set_description: "",
-    set_category: "",
-    flashcards: [],
-    username: "",
-    organization: "",
-  });
+  const [flashcards, setFlashcards] = useState<
+    {
+      confidence: number;
+      definition: string;
+      flashcard_id: string;
+      term: string;
+    }[]
+  >([]);
   const [currentFlashcardIndex, setCurrentFlashcardIndex] = useState(0);
   const [previousFlashcardIndex, setPreviousFlashcardIndex] = useState(-1);
   const [correctDefinition, setCorrectDefinition] = useState("");
@@ -31,26 +31,17 @@ const StudyComponent = () => {
 
   useEffect(() => {
     if (id!.length === 0 || id!.length !== 26 || id!.includes(" ")) {
-      setFlashcards((prevFlashcards) => ({
-        ...prevFlashcards,
-        set_name: "Хм, този сет не съществува",
-        set_description: "Провери дали си въвел правилния линк",
-        set_category: "",
-        flashcards: [],
-        username: "все още никого :<",
-        organization: "",
-      }));
       return;
     }
 
     instance
-      .get(`/sets/${id}`)
+      .get(`/sets/${id}/study`)
       .then((res) => {
-        setFlashcards(res.data.set);
-        if (flashcards.flashcards.length > 0) {
+        setFlashcards(res.data.flashcards);
+        if (flashcards.length > 0) {
           setCurrentFlashcardIndex(0);
-          setCorrectDefinition(flashcards.flashcards[0].definition);
-          shuffleDefinitions(flashcards.flashcards[0].definition);
+          setCorrectDefinition(flashcards[0].definition);
+          shuffleDefinitions(flashcards[0].definition);
         }
       })
       .catch((err) => {
@@ -78,100 +69,56 @@ const StudyComponent = () => {
   }, [currentFlashcardIndex]);
 
   const ensureDifferentFlashcard = () => {
+    if (flashcards.length === 0) {
+      return;
+    }
+
     let nextIndex;
     do {
-      nextIndex = Math.floor(Math.random() * flashcards.flashcards.length);
+      nextIndex = shuffleWeightedArray(flashcards);
     } while (nextIndex === previousFlashcardIndex);
 
     setCurrentFlashcardIndex(nextIndex);
   };
 
   const shuffleDefinitions = (correctDefinition: string) => {
-    const allDefinitions = [
-      ...flashcards.flashcards.map((card) => card.definition),
-    ];
+    const allDefinitions = [...flashcards.map((card) => card.definition)];
     const shuffled = shuffleArray([...allDefinitions, correctDefinition]);
     const selectedDefinitions = shuffled.slice(0, 4);
     setShuffledDefinitions(selectedDefinitions);
   };
 
   const handleAnswerButtonClick = (definition: string, isInput: boolean) => {
-    if (isInput) {
-      const updatedFlashcard = {
-        definition: flashcards.flashcards[currentFlashcardIndex].definition,
-        flashcard_id: flashcards.flashcards[currentFlashcardIndex].flashcard_id,
-        notes: null,
-        term: flashcards.flashcards[currentFlashcardIndex].term,
-        correctness: isAnswerCorrect(definition, correctDefinition) ? 1 : 0,
-        username: username,
-        user_id: userId,
-      };
+    const isCorrect = isInput
+      ? isAnswerCorrect(definition, correctDefinition)
+        ? 1
+        : 0
+      : definition === correctDefinition
+      ? 1
+      : 0;
 
+    const updatedFlashcard = {
+      correctness: isCorrect,
+      username: username,
+      user_id: userId,
+    };
+
+    const nextIndex = currentFlashcardIndex + 1;
+
+    if (flashcards[currentFlashcardIndex]) {
       instance
         .put(
-          `/flashcards/${flashcards.flashcards[currentFlashcardIndex].flashcard_id}/study`,
+          `/flashcards/${flashcards[currentFlashcardIndex].flashcard_id}/study`,
           updatedFlashcard
         )
         .catch((err) => console.error(err));
+    }
 
-      setPreviousFlashcardIndex(currentFlashcardIndex);
-      ensureDifferentFlashcard();
-      setCorrectDefinition(flashcards.flashcards[nextIndex].definition);
-      shuffleDefinitions(flashcards.flashcards[nextIndex].definition);
-    } else {
-      console.log(definition, correctDefinition);
-      if (definition === correctDefinition) {
-        const nextIndex = currentFlashcardIndex + 1;
-        if (nextIndex < flashcards.flashcards.length) {
-          const updatedFlashcard = {
-            definition: flashcards.flashcards[currentFlashcardIndex].definition,
-            flashcard_id:
-              flashcards.flashcards[currentFlashcardIndex].flashcard_id,
-            notes: null,
-            term: flashcards.flashcards[currentFlashcardIndex].term,
-            correctness: 1,
-            username: username,
-            user_id: userId,
-          };
-
-          instance
-            .put(
-              `/flashcards/${flashcards.flashcards[currentFlashcardIndex].flashcard_id}/study`,
-              updatedFlashcard
-            )
-            .catch((err) => console.error(err));
-
-          setPreviousFlashcardIndex(currentFlashcardIndex);
-          ensureDifferentFlashcard();
-          setCorrectDefinition(flashcards.flashcards[nextIndex].definition);
-          shuffleDefinitions(flashcards.flashcards[nextIndex].definition);
-        } else {
-          console.log("Reached end of flashcards");
-        }
-      } else {
-        const updatedFlashcard = {
-          definition: flashcards.flashcards[currentFlashcardIndex].definition,
-          flashcard_id:
-            flashcards.flashcards[currentFlashcardIndex].flashcard_id,
-          notes: null,
-          term: flashcards.flashcards[currentFlashcardIndex].term,
-          correctness: 0,
-          username: username,
-          user_id: userId,
-        };
-
-        instance
-          .put(
-            `/flashcards/${flashcards.flashcards[currentFlashcardIndex].flashcard_id}/study`,
-            updatedFlashcard
-          )
-          .catch((err) => console.error(err));
-
-        setPreviousFlashcardIndex(currentFlashcardIndex);
-        ensureDifferentFlashcard();
-        setCorrectDefinition(flashcards.flashcards[nextIndex].definition);
-        shuffleDefinitions(flashcards.flashcards[nextIndex].definition);
-      }
+    setPreviousFlashcardIndex(currentFlashcardIndex);
+    ensureDifferentFlashcard();
+    if (flashcards[nextIndex]) {
+      setCorrectDefinition(flashcards[nextIndex].definition);
+      shuffleDefinitions(flashcards[nextIndex].definition);
     }
   };
 
@@ -188,36 +135,40 @@ const StudyComponent = () => {
   };
 
   const shouldBeInput = (flashcards) => {
-    if (!flashcards || !flashcards.set || !flashcards.flashcards) {
+    if (!flashcards) {
       return [];
     }
 
     const avgConfidence =
-      flashcards.flashcards.reduce((sum, card) => sum + card.confidence, 0) /
-      flashcards.flashcards.length;
-    const result = flashcards.flashcards.map(
-      (card) => card.confidence > avgConfidence
-    );
+      flashcards.reduce((sum, card) => sum + card.confidence, 0) /
+      flashcards.length;
+    const result = flashcards.map((card) => card.confidence > avgConfidence);
 
     return result;
   };
 
-  const isInput = shouldBeInput(flashcards.flashcards);
+  const isInput = shouldBeInput(flashcards);
 
-  const shuffleArrayWeighted = (flashcards) => {
-    const weights = flashcards.map((card) => card.correctness);
-    const adjustedWeights = weights.map((weight) => Math.exp(-weight));
-    const totalWeight = adjustedWeights.reduce((acc, curr) => acc + curr, 0);
-    const randomWeight = Math.random() * totalWeight;
-
-    let cumulativeWeight = 0;
-    for (let i = 0; i < flashcards.length; i++) {
-      cumulativeWeight += adjustedWeights[i];
-      if (randomWeight <= cumulativeWeight) {
-        return i;
-      }
+  const shuffleWeightedArray = (flashcards) => {
+    if (!flashcards || flashcards.length === 0) {
+      return [];
     }
-    return flashcards.length - 1;
+
+    const meanConfidence =
+      flashcards.reduce((sum, card) => sum + card.confidence, 0) /
+      flashcards.length;
+
+    const shuffledFlashcards = shuffleArray(flashcards);
+
+    const shuffledWithInput = shuffledFlashcards
+      .filter((card) => card.confidence > meanConfidence)
+      .slice(0, 1);
+
+    const shuffledWithButtons = shuffledFlashcards
+      .filter((card) => card.confidence <= meanConfidence)
+      .slice(0, 3);
+
+    return [...shuffledWithInput, ...shuffledWithButtons];
   };
 
   // Levenshtein distance algorithm for string similarity (MOST LIKELY WILL CHANGE.)
@@ -265,41 +216,43 @@ const StudyComponent = () => {
   return (
     <>
       <Dashboard>
-      <div className="study-component">
-        <div className="study-wrapper">
-        {flashcards.flashcards.length > 0 && (
-          <div id="flashcard" className={"no-image-flashcard"}>
-            <div className="term">
-              {/* <p className="term-text">Термин:</p> */}
-              <h3>{parse(flashcards.flashcards[currentFlashcardIndex].term)}</h3>
-            </div>
-          </div>
-        )}
+        <div className="study-component">
+          <div className="study-wrapper">
+            {flashcards.length > 0 && (
+              <div id="flashcard" className={"no-image-flashcard"}>
+                <div className="term">
+                  {/* <p className="term-text">Термин:</p> */}
+                  <h3>{parse(flashcards[currentFlashcardIndex].term)}</h3>
+                </div>
+              </div>
+            )}
 
-        <div className="answer-options">
-          {flashcards.flashcards.slice(0, 4).map((flashcard, index) => (
-            <div className="option" key={flashcard.flashcard_id}>
-              {isInput[index] ? (
-                <input
-                  type="text"
-                  placeholder={`Въведи дефиниция за ${flashcard.term}`}
-                  onBlur={(e) => handleAnswerButtonClick(e.target.value, true)}
-                />
-              ) : (
-                <button
-                  key={index}
-                  onClick={() =>
-                    handleAnswerButtonClick(flashcard.definition, false)
-                  }
-                >
-                  {parse(flashcard.definition)}
-                </button>
-              )}
+            <div className="answer-options">
+              {flashcards.slice(0, 4).map((flashcard, index) => (
+                <div className="option" key={flashcard.flashcard_id}>
+                  {isInput[index] ? (
+                    <input
+                      type="text"
+                      placeholder={`Въведи дефиниция за ${flashcard.term}`}
+                      onBlur={(e) =>
+                        handleAnswerButtonClick(e.target.value, true)
+                      }
+                    />
+                  ) : (
+                    <button
+                      key={index}
+                      onClick={() =>
+                        handleAnswerButtonClick(flashcard.definition, false)
+                      }
+                    >
+                      {parse(flashcard.definition)}
+                    </button>
+                  )}
+                </div>
+              ))}
             </div>
-          ))}
           </div>
         </div>
-      </div>
       </Dashboard>
     </>
   );
