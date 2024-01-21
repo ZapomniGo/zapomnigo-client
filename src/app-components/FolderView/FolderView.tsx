72cf678e350b59b5912dceae5e4dc3de51ff7266
Aleks Ivanov <al.ivanov1908@gmail.com>
2024-01-21 16:28:40 +0200

    fix icon in folder view

--- src/app-components/FolderView/FolderView.tsx
+++ src/app-components/FolderView/FolderView.tsx
@@ -54,7 +54,7 @@ export const FolderView: React.FC = () => {
                 title={card.set_name}
                 description={card.set_description}
                 institution={card.organization_name}
-                image={'/public/logo.jpg'}
+                image={'/logo.jpg'}
                 creator_name={card.username}
                 onMouseEnter={handleMouseEnter}
                 onMouseLeave={handleMouseLeave}
