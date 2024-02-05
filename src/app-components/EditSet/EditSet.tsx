2f91d26f9587ee9b47f35a5279cf74ed29e5f430
Aleks Ivanov <al.ivanov1908@gmail.com>
2024-02-05 02:35:32 +0200

    fix navigate commented

--- src/app-components/EditSet/EditSet.tsx
+++ src/app-components/EditSet/EditSet.tsx
@@ -185,7 +185,7 @@ export const EditSet = () => {
       })
       .then((response) => {
         toast("Редакцията е готова");
-        // navigate("/app/set/" + id);
+        navigate("/app/set/" + id);
         window.scrollTo(0, 0);
       })
       .catch((error) => {
