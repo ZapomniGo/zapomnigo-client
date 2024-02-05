7341ca4c254e1d8b521b839ae6f5c78e30718681
Aleks Ivanov <al.ivanov1908@gmail.com>
2024-02-05 02:13:35 +0200

    added categories subcategories to mysets

--- src/app-components/Sets/Sets.tsx
+++ src/app-components/Sets/Sets.tsx
@@ -64,6 +64,8 @@ export const Sets: React.FC = () => {
               title={card.set_name}
               description={card.set_description}
               institution={card.organization_name}
+              category={card.category_name}
+              subcategory={card.subcategory_name}
               image={"/logo.jpg"}
               creator_name={card.username}
               onMouseEnter={handleMouseEnter}
