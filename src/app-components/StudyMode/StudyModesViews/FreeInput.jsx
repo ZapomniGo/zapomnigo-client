d9cb04d31fd813283566b109bd318e9476ca9d30
Atanas Bobev <bobevatanas@gmail.com>
2024-02-07 09:38:03 +0200

    Remove commented out code in FreeInput.jsx

--- src/app-components/StudyMode/StudyModesViews/FreeInput.jsx
+++ src/app-components/StudyMode/StudyModesViews/FreeInput.jsx
@@ -19,9 +19,6 @@ const FreeInput = (props) => {
     };
   }, [answer]);
 
-  // useEffect(() => {
-  //   console.log(showResults);
-  // }, [showResults]);
 
   React.useEffect(() => {
     setAnswer("");
