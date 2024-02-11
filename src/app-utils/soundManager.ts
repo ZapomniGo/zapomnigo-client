9e5c71ff2e4db4bd1995499ba67d5352a1e2b31c
Atanas Bobev <bobevatanas@gmail.com>
2024-02-11 17:17:21 +0200

    Refactor soundManager imports and use LazyAudio class

--- src/app-utils/soundManager.ts
+++ src/app-utils/soundManager.ts
@@ -1,18 +1,45 @@
+class LazyAudio {
+  private audio: HTMLAudioElement | null = null;
+  private src: string;
+
+  constructor(src: string) {
+    this.src = src;
+  }
+
+  play() {
+    if (!this.audio) {
+      this.audio = new Audio(this.src);
+    }
+    this.audio.play();
+  }
+  pause() {
+    if (this.audio) {
+      this.audio.pause();
+    }
+  }
+  stop() {
+    if (this.audio) {
+      this.audio.pause();
+      this.audio.currentTime = 0;
+    }
+  }
+}
+
 //Flip Close
-const FC = new Audio("../../Sounds/FC.wav");
+const FC = new LazyAudio("../../Sounds/FC.wav");
 //Flip Open
-const FO = new Audio("../../Sounds/FO.wav");
+const FO = new LazyAudio("../../Sounds/FO.wav");
 //Long Negative
-const LN = new Audio("../../Sounds/LN.wav");
+const LN = new LazyAudio("../../Sounds/LN.wav");
 //Long Positive
-const LP = new Audio("../../Sounds/LP.wav");
+const LP = new LazyAudio("../../Sounds/LP.wav");
 //Medium Negative
-const MN = new Audio("../../Sounds/MN.wav");
+const MN = new LazyAudio("../../Sounds/MN.wav");
 //Medium Positive
-const MP = new Audio("../../Sounds/MP.wav");
+const MP = new LazyAudio("../../Sounds/MP.wav");
 //Short Negative
-const SN = new Audio("../../Sounds/SN.wav");
+const SN = new LazyAudio("../../Sounds/SN.wav");
 //Short Positive
-const SP = new Audio("../../Sounds/SP.wav");
+const SP = new LazyAudio("../../Sounds/SP.wav");
 
 export { FC, FO, LN, LP, MN, MP, SN, SP };
